34cee0490972f714209046e5ccf5861444d71119
Ioana Pavelescu <ioana_pavelescu@yahoo.com>
2011-03-18 11:04:24 +0100

    Adding buttons and textField

--- HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
+++ HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
@@ -2,16 +2,48 @@ package com.example.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HelloAndroid extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android");
-        setContentView(tv);
+      //  TextView tv = new TextView(this);
+       // tv.setText("Hello, Android");
+        setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+            	final EditText edittext = (EditText) findViewById(R.id.edittext);
+            	edittext.setText("Welcome to the Android world, "+(edittext.getText().toString())+" !");
 
+            }
+        });
+        
+      //Cancel Button
+        final Button cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		
+        		// Perform action on click
+        		EditText et = (EditText) findViewById(R.id.edittext);
+        		//et.append(" Sree= " + et.getText().toString());
+        	
+        		et.setText("");
+        	}
+        
+        });
+    
     }
+    
 }
